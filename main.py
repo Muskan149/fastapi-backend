be38243d5eba31a0372b62fb03f7f6b69e52b7e8
Muskan Mahajan <58877651+Muskan149@users.noreply.github.com>
2025-06-15 12:55:25 -0700

    removed port

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ async def perform_index_profile(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    # port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
