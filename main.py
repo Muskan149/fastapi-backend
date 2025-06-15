7008b79eab8c0f13b5f24104fae8d95294653d23
Muskan Mahajan <58877651+Muskan149@users.noreply.github.com>
2025-06-15 11:52:05 -0700

    changed summarizer

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from create_embeddings import fetch_top_k_profiles, index_profile
 from pydantic import BaseModel
 from typing import Dict, Any
+import os
 
 class ProfileResponse(BaseModel):
     message: str
@@ -52,5 +53,5 @@ async def perform_index_profile(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
